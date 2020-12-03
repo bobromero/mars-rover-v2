e93c7b31c5ef0f52b76e61eaede5d766fd9738b3
rromero123102 <rromero21332@columbushs.com>
2020-12-03 13:48:39 -0500

    added rover and functionality

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,25 @@
 // Rover Object Goes Here
 // ======================
 let rover = {
-  direction: "N"
+  direction: "N",
+  x: 0,
+  y: 0,
+  travelLog:[]
 }
 console.log(rover.direction);
+
+let grid = [
+  [null,"obstacle",null,null,null,null,null,null,null,null],
+  [null,"Rover",null,null,"obstacle",null,null,null,null,null],
+  [null,null,null,null,null,null,null,null,null,null],
+  ["obstacle",null,null,null,null,null,null,"obstacle",null,null],
+  [null,null,"obstacle",null,"Rover",null,null,null,null,null],
+  [null,null,null,null,null,null,null,null,null,null],
+  [null,null,null,null,"obstacle",null,"obstacle",null,null,null],
+  [null,"Rover",null,null,null,null,null,null,null,null],
+  [null,null,null,null,null,null,null,null,null,null],
+  [null,null,null,"obstacle",null,null,null,null,null,null],
+]
 
 // ======================
 function turnLeft(rover){
@@ -48,7 +64,122 @@ function turnRight(rover){
   console.log(rover.direction);
 }
 
-function moveForward(rover){
+function moveForward(rover,grid){
   console.log("moveForward was called");
+  switch(rover.direction) {
+      case "N":
+        
+        if (rover.y > 0 && grid[rover.x][rover.y-1] == null ){
+          rover.y -= 1;
+        }
+        else if(grid[rover.x][rover.y-1] == "rover"){
+          console.log("you're pushing into a rover");
+        }
+        else{
+          console.log("can't move, obstacle");
+        }
+        
+        break;
+
+      case "E":
+        //change 10 to array.length or whatever its called
+        if (rover.x < grid.length && grid[rover.x + 1][rover.y] == null){
+          rover.x += 1;
+        }
+        else if(grid[rover.x + 1][rover.y] == "rover"){
+          console.log("you're pushing into a rover");
+        }
+        else{
+          console.log("can't move, obstacle");
+        }
+        break;
+
+      case "S":
+        //change 10 to array.length or whatever its called
+        if (rover.y < grid.length && grid[rover.x][rover.y + 1] == "rover"){
+          rover.y += 1;
+        }
+        else if(grid[rover.x][rover.y + 1] == "rover"){
+          console.log("you're pushing into a rover");
+        }
+        else{
+          console.log("can't move, obstacle");
+        }
+        break;
+
+      case "W":
+        if (rover.x > 0 && grid[rover.x - 1][rover.y] == null){
+          rover.x -= 1;
+        }
+        else if(grid[rover.x - 1][rover.y] == "rover"){
+          console.log("you're pushing into a rover");
+        }
+        else{
+          console.log("can't move, obstacle");
+        }
+        break;
+        
+      default:
+        console.log("can't move");
+    }
   
+  console.log(rover.direction);
+  console.log("posion: " + rover.x + " " + rover.y);
+  rover.travelLog.push(rover.x , rover.y);
+}
+
+function moveBackward(rover){
+  console.log("moveForward was called");
+  switch(rover.direction) {
+    case "N":
+      
+      if (rover.y > 0){
+        rover.y += 1;
+      }
+      
+      break;
+    case "E":
+      //change 10 to array.length or whatever its called
+      if (rover.x < 10){
+        rover.x -= 1;
+      }
+      break;
+    case "S":
+      //change 10 to array.length or whatever its called
+      if (rover.y < 10){
+        rover.y -= 1;
+      }
+      break;
+    case "W":
+      if (rover.x > 0){
+        rover.x += 1;
+      }
+      break;
+    default:
+      console.log("can't move");
+  }
+  console.log(rover.direction);
+  console.log("posion: " + rover.x + " " + rover.y);
+}
+
+function commands(str){
+  for (var i = 0; i < str.length; i++) {
+    switch (str.charAt(i)){
+      case "f":
+        moveForward(rover,grid);
+        break;
+      case "r":
+        turnRight(rover);
+        break;
+      case "l":
+        turnLeft(rover);
+        break;
+      case "b":
+        moveBackward(rover,grid);
+        break;
+    default:
+      console.log("Incorrect input recieved.")
+    }
+    
+  }
 }
